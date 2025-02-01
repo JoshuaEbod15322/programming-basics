a4e20a28a81911681123eaf8a00f3260148d0c77
JoshuaEbod15322 <joshuaebod.202300287@gmail.com>
2025-02-01 10:06:05 +0800

    updated

--- script.js
+++ script.js
@@ -2,20 +2,25 @@
 let name = "Joshua";
 let age = 20;
 let isStudent = true;
-console.log(`Name: ${name}, Age: ${age}, Student: ${isStudent}`);
+output += `Name: ${name}, Age: ${age}, Student: ${isStudent}\n`;
+
 // Loops
 for (let i = 1; i <= 10; i++) {
- console.log(i);
+    output += i + "\n";
 }
+
 // Functions
 function addNumbers(a, b) {
- return a + b;
+    return a + b;
 }
-console.log(addNumbers(5, 10));
+output += addNumbers(5, 10) + "\n";
+
 // Conditionals
 if (age >= 18) {
- console.log("Adult");
+    output += "Adult\n";
 } else {
- console.log("Minor");
+    output += "Minor\n";
 }
-document.getElementById(`console-output`).innerText = output;  
+
+// Display the output in the HTML
+document.getElementById('console-output').innerText = output;
